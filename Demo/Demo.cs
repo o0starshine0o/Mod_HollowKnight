9e4d0eabd1a80e2763988cabaa7592230244af7d
aberhu <aberhu@tencent.com>
2022-11-06 17:35:42 +0800

    完成了knight的位置输出

--- Demo/Demo.cs
+++ Demo/Demo.cs
@@ -1,18 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using GlobalEnums;
 using Modding;
+using UnityEngine;
 
 namespace Demo {
-    public class ClassTest : Mod {
+    public class DqnMod : Mod {
 
-        private const int _modVersion = 3;
+        // 使用单例, 方便其他位置调取
+        public static DqnMod instance;
+
+        private const int _modVersion = 14;
+
+        public DqnMod ()
+        {
+            instance = this;
+        }
 
         public override string GetVersion () => $"{ModHooks.ModVersion}:{_modVersion}";
 
         public override void Initialize ()
         {
             Log ("Hello World");
+
+            GUIController.Instance.Update ();
 
             ModHooks.SoulGainHook += SoulGainHook;
 
@@ -21,6 +33,16 @@ namespace Demo {
             ModHooks.AfterPlayerDeadHook += AfterPlayerDeadHook;
 
             ModHooks.AfterTakeDamageHook += AfterTakeDamageHook;
+
+            ModHooks.AttackHook += AttackHook;
+
+            ModHooks.BeforeSceneLoadHook += BeforeSceneLoadHook;
+
+            ModHooks.CharmUpdateHook += CharmUpdateHook;
+
+            ModHooks.ColliderCreateHook += ColliderCreateHook;
+
+            ModHooks.DrawBlackBordersHook += DrawBlackBordersHook;
         }
 
         private int SoulGainHook (int soul)
@@ -44,9 +66,44 @@ namespace Demo {
 
         private int AfterTakeDamageHook (int hazardType, int damageAmount)
         {
-            // 没啥用， 没有入参，没有返回值
-            Log ($"AfterTakeDamageHook{hazardType}: {damageAmount}");
+            // 被BOSS攻击到的回调， type-1， damage-2
+            Log ($"AfterTakeDamageHook({hazardType}): {damageAmount}");
             return damageAmount;
+        }
+
+        private void AttackHook (AttackDirection attackDirection)
+        {
+            Log ($"AttackHook: {attackDirection}");
+        }
+
+        private string BeforeSceneLoadHook (string scene)
+        {
+            // 进入场景回调，可以考虑脚本控制角色或者开始AI
+            // GG, GODS_GLORY, 神居
+            // GG_Atrium， 中庭
+            // GG_Workshop, 作坊
+            // GG_Hornet_2， 大黄蜂
+            Log ($"BeforeSceneLoadHook: {scene}");
+            return scene;
+        }
+
+        private void CharmUpdateHook (PlayerData data, HeroController controller)
+        {
+            // 护符更新时回调
+            // PlayerData数据量很多, HeroController数据量也很多
+            Log ($"CharmUpdateHook: {data}, {controller}");
+        }
+
+        private void ColliderCreateHook (GameObject gameObject) =>
+            HitBox.Instance.UpdateHitBox (gameObject);
+
+
+        private void DrawBlackBordersHook (List<GameObject> gameObjects)
+        {
+            // 目前还没看到哪里有使用, 先放着吧, 以后再看
+            foreach (GameObject game in gameObjects) {
+                Log ($"DrawBlackBordersHook: {game.name}");
+            }
         }
     }
 }
